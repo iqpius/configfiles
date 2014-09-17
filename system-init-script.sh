7ff7f378571a3cd5d16040a08d563582d35f651b
Jose GL <jose@josegl.com>
2014-09-17 10:17:22 +0000

    Agregadas algunas tareas que aun tengo que hacer

--- system-init-script.sh
+++ system-init-script.sh
@@ -8,9 +8,15 @@ git config --global core.editor "vim"
 git config --global user.name "Jose GL" 
 git config --global user.email jose@josegl.com
 
-
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc 
-cp .tmux.conf ~/.tmux.conf
+
+# Poner la configuracion de tmux cuando la tenga. 
+# Poner las siguientes configuraciones:
+
+# control A como inicio de escape
+# control A jklh para moverme entre las ventanas de tmux
+# control A % para partir verticalmente
+# control A # para partir horizontalmente
 
 
