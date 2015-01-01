23f849efd2c2e498688164e36b90a738f4c2853c
Jose G.L <jose@josegl.com>
2015-01-01 13:45:39 +0100

    Nuevas configs automatizadas

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -7,14 +7,16 @@ git config --global user.name "Jose GL"
 git config --global user.email jose@josegl.com
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Restauracion de las configs
+cp -r .oh-my-zsh ~/
+cp  .zshrc ~/
+cp  .zsh-update ~/
+cp -r .oh-my-zsh ~/
 cp .vimrc ~/.vimrc 
+cp -r .themes ~/
+cp -r .icons ~/
+cp .tmux.conf ~/
+cp -r .fonts ~/
 
-# Pongo oh my zsh
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-# Poner la configuracion de tmux cuando la tenga. 
-# Poner las siguientes configuraciones:
-
-# control A como inicio de escape
-# control A jklh para moverme entre las ventanas de tmux
-# control A % para partir verticalmente
-# control A # para partir horizontalmente
+echo "No olvides restaurar la llave ssh"
