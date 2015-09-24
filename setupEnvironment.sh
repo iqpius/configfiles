ce167f5a0aeb9b6f1c4dbe78dc86fb9b2b86a32b
Jose G.L <jose@josgl.com>
2015-09-24 13:00:44 +0200

    Agregado metodo para obtener automaticamente solarized colors

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -7,6 +7,8 @@ git config --global user.name "Jose GL"
 git config --global user.email jose@josegl.com
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/altercation/vim-colors-solarized.git
+cp -r vim-colors-solarized/colors ~/.vim/
 
 # Restauracion de las configs
 cp  .zshrc ~/
