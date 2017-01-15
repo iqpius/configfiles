5d9778899458c0168166515896f585263491ccdb
Jose GL <jgarloz18@gmail.com>
2017-01-15 20:22:35 +0000

    Changed symbolic links to hard copies in setup

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -5,11 +5,9 @@ git config --global core.editor "vis"
 git config --global user.name "Jose GL" 
 git config --global user.email jgarloz18@gmail.com
 
-if [ -d ~/.config ]; then;
-  ln -s vis ~/.config/
-else
-  mkdir ~/.config
-  ln -s vis ~/.config/
+if ! [ -d ~/.config ]
+then
+  mkdir -p ~/.config
 fi
-
-ln -s .tmux.conf ~/.tmux.conf
+cp -r vis ~/.config/
+cp .tmux.conf ~/.tmux.conf
